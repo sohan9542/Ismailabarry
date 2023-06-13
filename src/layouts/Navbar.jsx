45a3e9e867ab27351c96d456934184fa4756a42f
sohan954201~ <marufislam9542@gmail.com>
2023-06-13 10:46:36 +0600

    jangflix mobile fixed

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -96,11 +96,7 @@ const Navbar = () => {
           <div className="flex items-center justify-between">
             <a href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
-              <img
-                className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                alt=""
-              />
+              <img className="w-12" src="/logo.png" alt="" />
             </a>
             <button
               type="button"
